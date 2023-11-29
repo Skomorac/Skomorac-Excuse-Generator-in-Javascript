9dc1564c18a97c8fff5b0c28a82951bc85cb5f6d
Admir Skomorac <56019597+Skomorac@users.noreply.github.com>
2023-11-29 12:29:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Randomly select one item from each array
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Concatenate the selected items to form the excuse
+  let excuse =
+    `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}` + " !!!";
+
+  // Set the excuse into the HTML element with id "excuse"
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
